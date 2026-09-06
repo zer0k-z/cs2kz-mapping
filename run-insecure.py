82aff302c258db9e9f76e16894249e1935349215
zer0.k <zer0kams@gmail.com>
2026-09-06 18:48:33 +0200

    allow flat file addons for -insecure

--- run-insecure.py
+++ run-insecure.py
@@ -7,6 +7,14 @@ if __name__ == '__main__':
         time.sleep(3)
         exit()
 
+    gameinfo_path, backup_path, core_gameinfo_path, core_backup_path = backup_files(path)
+
+    modify_gameinfo_flat_file_addons(gameinfo_path)
+
     cs2 = os.path.join(path, 'game', 'bin', 'win64', 'cs2.exe')
     print(f"Launching CS2 insecure instance from '{cs2}'...")
     process = subprocess.Popen([cs2, '-insecure'])
+
+    time.sleep(1)
+
+    restore_files(backup_path, gameinfo_path, core_backup_path, core_gameinfo_path)
